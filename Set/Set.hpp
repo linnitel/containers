822872a53465c5391fa1892b646c4b2ce6114ab0
Julia Martsenko <jmartsenko@gmil.com>
2021-07-13 15:48:39 +0300

    added header to stack, deleted folders with unrelevant containers and added set

---
+++ Set/Set.hpp
@@ -0,0 +1,16 @@
+
+#ifndef SET_HPP
+# define SET_HPP
+
+namespace ft {
+
+	template <typename T>
+	class Set {
+	public:
+		// Typedefs -----
+		typedef T value_type;
+
+	};
+}
+
+#endif
--- Stack/Stack.hpp
+++ Stack/Stack.hpp
@@ -3,10 +3,44 @@
 
 namespace ft {
 
-	template <typename T>
+	template <class T, class Container = Vector<T>>
 	class Stack {
+	public:
+		// Typedefs -----
+		typedef T value_type;
+		typedef Container container_type;
+		typedef size_t size_type;
+	private:
+		// Variables -----
+		container_type _stack;
 
+	public:
+		// Constructors -----
+		explicit stack (const container_type &ctnr = container_type());
+
+		// Member functions -----
+		bool empty() const;
+		size_type size() const;
+
+		value_type& top();
+		const value_type& top() const;
+
+		void push (const value_type& val);
+		void pop();
 	};
+
+	template <class T, class Container>
+	bool operator== (const stack<T,Container>& lhs, const stack<T,Container>& rhs);
+	template <class T, class Container>
+	bool operator!= (const stack<T,Container>& lhs, const stack<T,Container>& rhs);
+	template <class T, class Container>
+	bool operator<  (const stack<T,Container>& lhs, const stack<T,Container>& rhs);
+	template <class T, class Container>
+	bool operator<= (const stack<T,Container>& lhs, const stack<T,Container>& rhs);
+	template <class T, class Container>
+	bool operator>  (const stack<T,Container>& lhs, const stack<T,Container>& rhs);
+	template <class T, class Container>
+	bool operator>= (const stack<T,Container>& lhs, const stack<T,Container>& rhs);
 }
 
 #endif
