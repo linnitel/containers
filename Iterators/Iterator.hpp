3cf8d12db3254f3f147c02223ff772e1dc65c441
Julia Martsenko <jmartsenko@gmil.com>
2021-07-20 01:11:24 +0300

    finished InputIterator class

--- Iterators/Iterator.hpp
+++ Iterators/Iterator.hpp
@@ -54,17 +54,38 @@ namespace ft {
 		InputIterator(pointer ptr): iterator(ptr) {};
 		InputIterator(InputIterator const &InpIter): iterator(InpIter._it) {};
 		InputIterator &operator=(InputIterator const &InpIter) {
-			iterator::_it = InpIter._it;
-			iterator::_n = InpIter._n;
+			this->_it = InpIter._it;
+			this->_n = InpIter._n;
 			return *this;
 		}
 		friend bool operator== (const InputIterator& a, const InputIterator& b);
 		friend bool operator!= (const InputIterator& a, const InputIterator& b);
+
+		pointer operator->() { return (*this)._it; };
+		InputIterator operator++(int) {
+			InputIterator tmp = *this;
+			++(*this);
+			return tmp;
+		};
 	};
 
 	template <class T, class Pointer = T*, class Reference = T&>
 	bool operator== (const InputIterator<T, Pointer, Reference>& a, const InputIterator<T, Pointer, Reference>& b) { return a._it == b._it; }
 	template <class T, class Pointer = T*, class Reference = T&>
-	bool operator!= (const InputIterator<T, Pointer, Reference>& a, const InputIterator<T, Pointer, Reference>& b) { return a._it != b._it; }
+	bool operator!= (const InputIterator<T, Pointer, Reference>& a, const InputIterator<T, Pointer, Reference>& b) { return !(a._it == b._it); }
+
+	template<class Category,            // Iterator::iterator_category
+			class T,                     // Iterator::value_type
+			class Distance = std::ptrdiff_t,  // Iterator::difference_type
+			class Pointer = T *,          // Iterator::pointer
+			class Reference = T &>        // Iterator::reference
+	class ForwardIterator: public InputIterator<Category, T, Distance, Pointer, Reference> {
+		typedef InputIterator<Category, T, Distance, Pointer, Reference> input_iterator;
+		typedef typename input_iterator::value_type value_type;
+		typedef typename input_iterator::difference_type difference_type;
+		typedef typename input_iterator::pointer pointer;
+		typedef typename input_iterator::reference reference;
+		typedef typename input_iterator::iterator_category iterator_category;
+	};
 }
 #endif
